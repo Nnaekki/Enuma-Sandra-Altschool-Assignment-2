a0ebeebf77b02b22391a2cc76224fce6306c5b9e
Sandra Enuma <105364786+Nnaekki@users.noreply.github.com>
2022-07-20 20:09:39 +0000

    Sum of numbers

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,14 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+let sum = 0;
+  for (let i=0; i<arrayOfNumbers.length; i++)
+    sum += arrayOfNumbers[i];
+  return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+
+let arrayOfNumbers = [230, 330, 430, 530, 630];
+   console.log(sumOfNumbers(arrayOfNumbers));
 
 /**
  * Challenge - 2
